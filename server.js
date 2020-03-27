8ea21a6aee61bc39cc5da942d41216f7141d9125
Somil112 <somilshah112@gmail.com>
2020-03-27 18:34:32 +0530

    Basic Routing

--- server.js
+++ server.js
@@ -6,5 +6,12 @@ const connectDB = require("./config/db")
 // Connect Database
 connectDB()
 
+app.get("/", (req, res) => res.send("API Running"))
+
+app.use("/api/users", require("./routes/api/users"))
+app.use("/api/posts", require("./routes/api/posts"))
+app.use("/api/profile", require("./routes/api/profile"))
+app.use("/api/auth", require("./routes/api/auth"))
+
 // Make Server Online
 app.listen(PORT, () => console.log("Server Online!"))
