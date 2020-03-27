71a731e2dc0ef5928dd43596c01d8c042c74e71e
Somil112 <somilshah112@gmail.com>
2020-03-27 18:10:53 +0530

    DB Connection

---
+++ config/db.js
@@ -0,0 +1,22 @@
+// Used for Schema Building with Mongo
+const mongoose = require("mongoose")
+// Used to get Default Values
+const config = require("config")
+// Contains Mongo DB URI
+const db = config.get("mongoURI")
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
+    console.log("MongoDB connected")
+  } catch (err) {
+    console.log(err.message)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
