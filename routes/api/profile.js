5567ce75d4f32af26aea26e4f521096db70442ba
Somil112 <somilshah112@gmail.com>
2020-04-01 03:18:03 +0530

    Create Profile and Make a Get Profile Route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,22 @@
 const express = require("express")
 const router = express.Router()
+const auth = require("../../middleware/auth")
+const Profile = require("../../models/Profile")
 
-router.get("/", (req, res) => res.send("Profile Route"))
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate("user", ["name", "avatar"])
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" })
+    }
+
+    res.json(profile)
+  } catch (err) {
+    res.status(500).json("Server Error")
+  }
+})
 
 module.exports = router
