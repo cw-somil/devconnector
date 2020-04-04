8ddf5d22d20845ca9a9bb5099c025687647a7fd6
Somil112 <somilshah112@gmail.com>
2020-04-04 05:37:04 +0530

    Added Github Route to access Repos | Profile Route Complete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require("../../middleware/auth")
 const Profile = require("../../models/Profile")
 const { check, validationResult } = require("express-validator")
 const User = require("../../models/User")
+const config = require("config")
+const request = require("request")
 
 // Get Logged In User's Profile
 router.get("/me", auth, async (req, res) => {
@@ -251,4 +253,34 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     res.status(500).send("Server Error")
   }
 })
+
+// Get Github Repos
+// Public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    }
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error)
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No Github profile found" })
+      }
+
+      res.json(JSON.parse(body))
+    })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send("Server Error")
+  }
+})
+
 module.exports = router
